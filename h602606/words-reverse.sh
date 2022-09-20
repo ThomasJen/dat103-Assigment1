4ff0ff72559f8da5dd1371043ee7a1f541b05511
Thomas Anker Jensen <thakje8@gmail.com>
2022-09-20 11:40:58 +0200

    Fiksa shit

---
+++ h602606/words-reverse.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check for illegal arguments
+[[ ! -z $1 ]] && [[ "$1" != "--bypass" ]] && echo "Illegal argument '$1'" >&2 && exit 1
+
+# Store bypass flag in variable
+bypass=$1
+
+# Create unique hashdir name
+hash_dir="$(date +'%N')-hashes"
+
+# Create hashdir with unique name
+mkdir $hash_dir
+
+# Read from stdin
+text=$(</dev/stdin)
+
+# Depunctuate text
+text=$(echo "$text" | ./depunctuate.sh $hash_dir)
+
+# Reverse if not called with bypass flag
+if [[ "$bypass" != "--bypass" ]]; then
+  text=$(echo $text | ./words-reverse-ll)
+fi
+
+# Repunctuate text
+text=$(echo "$text" | ./repunctuate.sh $hash_dir)
+
+# Remove temp hash dir
+rm -rf $hash_dir
+
+# Print final output
+echo "$text"
